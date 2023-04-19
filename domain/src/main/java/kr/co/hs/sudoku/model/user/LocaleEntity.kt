6e83ea67beb5cce43bf2ac957f1677b0bf45bae4
hsbaewa <hsbaewa@gmail.com>
2023-04-19 11:17:05 +0900

    #58 LocaleEntity에 국가 flag 이모지 출력 하는 함수 추가

--- domain/src/main/java/kr/co/hs/sudoku/model/user/LocaleEntity.kt
+++ domain/src/main/java/kr/co/hs/sudoku/model/user/LocaleEntity.kt
@@ -1,6 +1,10 @@
 package kr.co.hs.sudoku.model.user
 
+import java.util.Locale
+
 interface LocaleEntity {
     val lang: String
     val region: String
+    fun toLocale(): Locale
+    fun getLocaleFlag(): String
 }
