f1df6a38098902a71c2caef2d749a5cc19489fe2
hsbaewa <hsbaewa@gmail.com>
2023-12-18 16:20:51 +0900

    #73 매트릭스 리스트 아이템 레이아웃 버그 수정

--- app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemViewHolder.kt
+++ app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemViewHolder.kt
@@ -1,38 +1,62 @@
 package kr.co.hs.sudoku.feature.matrixlist
 
+import android.view.View
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import androidx.core.view.updateLayoutParams
 import kr.co.hs.sudoku.core.ViewHolder
 import kr.co.hs.sudoku.databinding.LayoutMatrixListItemBinding
 import kr.co.hs.sudoku.model.matrix.IntMatrix
 import kr.co.hs.sudoku.views.MatrixItemView
 
-sealed class MatrixListItemViewHolder(binding: LayoutMatrixListItemBinding) :
+sealed class MatrixListItemViewHolder(val binding: LayoutMatrixListItemBinding) :
     ViewHolder(binding.root) {
 
-    abstract val matrixItemView: MatrixItemView
+    private lateinit var matrixItemView: MatrixItemView
 
-    fun onBind(item: IntMatrix) = matrixItemView.setMatrix(item)
+    fun onBind(item: IntMatrix) {
+        if (!this::matrixItemView.isInitialized) {
+            with(binding.cardView) {
+                matrixItemView = createMatrixItemView().also {
+                    addView(it)
+                    onMeasuredSize { w, h ->
+                        it.updateLayoutParams {
+                            width = w.minus(contentPaddingLeft.plus(contentPaddingRight))
+                        }
+                    }
+                }
+
+            }
+        }
+
+        matrixItemView.setMatrix(item)
+    }
+
+    private inline fun View.onMeasuredSize(crossinline onSize: (Int, Int) -> Unit) {
+        viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+                onSize(measuredWidth, measuredHeight)
+            }
+        })
+    }
+
+    protected abstract fun createMatrixItemView(): MatrixItemView
 
     class BeginnerItemViewHolder(binding: LayoutMatrixListItemBinding) :
         MatrixListItemViewHolder(binding) {
-        override val matrixItemView: MatrixItemView by lazy {
+        override fun createMatrixItemView() =
             MatrixItemView.BeginnerMatrixItemView(itemView.context)
-                .apply { binding.cardView.addView(this) }
-        }
     }
 
     class IntermediateItemViewHolder(binding: LayoutMatrixListItemBinding) :
         MatrixListItemViewHolder(binding) {
-        override val matrixItemView: MatrixItemView by lazy {
+        override fun createMatrixItemView() =
             MatrixItemView.IntermediateMatrixItemView(itemView.context)
-                .apply { binding.cardView.addView(this) }
-        }
     }
 
     class AdvancedItemViewHolder(binding: LayoutMatrixListItemBinding) :
         MatrixListItemViewHolder(binding) {
-        override val matrixItemView: MatrixItemView by lazy {
+        override fun createMatrixItemView() =
             MatrixItemView.AdvancedMatrixItemView(itemView.context)
-                .apply { binding.cardView.addView(this) }
-        }
     }
 }
