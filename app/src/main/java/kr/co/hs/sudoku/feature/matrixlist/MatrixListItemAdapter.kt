3dc9c31ed2d419ffb49022147b4ea18359d5a31e
hsbaewa <hsbaewa@gmail.com>
2023-12-20 10:53:41 +0900

    stage list item 레이아웃 파일 변경 및 필요없는 레이아웃 파일 제거

--- app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemAdapter.kt
+++ app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemAdapter.kt
@@ -3,15 +3,13 @@ package kr.co.hs.sudoku.feature.matrixlist
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import kr.co.hs.sudoku.core.ViewHolder
-import kr.co.hs.sudoku.databinding.LayoutMatrixListHeaderBinding
-import kr.co.hs.sudoku.databinding.LayoutMatrixListItemBinding
-import kr.co.hs.sudoku.databinding.LayoutMatrixListTitleBinding
 import kr.co.hs.sudoku.model.matrix.IntMatrix
 
 class MatrixListItemAdapter(
     private val onItemClick: (IntMatrix) -> Unit
-) : ListAdapter<MatrixListItem, ViewHolder>(MatrixListItemDiffCallback()) {
+) : ListAdapter<MatrixListItem, MatrixListItemViewHolder<out MatrixListItem>>(
+    MatrixListItemDiffCallback()
+) {
 
     companion object {
         const val VT_HEADER = 1010
@@ -19,44 +17,46 @@ class MatrixListItemAdapter(
         const val VT_TITLE = 1030
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MatrixListItemViewHolder<out MatrixListItem> {
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
-            VT_TITLE -> MatrixListTitleViewHolder(
-                LayoutMatrixListTitleBinding.inflate(inflater, parent, false)
-            )
+            VT_TITLE ->
+                MatrixListItemViewHolder.create<MatrixListItem.TitleItem>(inflater, parent)
 
-            VT_HEADER -> MatrixListHeaderViewHolder(
-                LayoutMatrixListHeaderBinding.inflate(inflater, parent, false)
-            )
+            VT_HEADER ->
+                MatrixListItemViewHolder.create<MatrixListItem.HeaderItem>(inflater, parent)
 
-            VT_ITEM -> MatrixListItemViewHolder(
-                LayoutMatrixListItemBinding.inflate(inflater, parent, false)
-            ).apply {
-                binding.cardView.setOnClickListener {
-                    (getItem(bindingAdapterPosition) as? MatrixListItem.MatrixItem)
-                        ?.matrix
-                        ?.let(onItemClick)
-                }
-            }
+            VT_ITEM ->
+                MatrixListItemViewHolder.create<MatrixListItem.MatrixItem>(inflater, parent)
+                    .apply {
+                        setOnClickListener {
+                            (getItem(bindingAdapterPosition) as? MatrixListItem.MatrixItem)
+                                ?.matrix
+                                ?.let(onItemClick)
+                        }
+                    }
 
             else -> throw Exception("invalid type")
         }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: MatrixListItemViewHolder<out MatrixListItem>,
+        position: Int
+    ) {
         when (val item = getItem(position)) {
-            is MatrixListItem.HeaderItem -> (holder as MatrixListHeaderViewHolder).onBind(item)
-            is MatrixListItem.MatrixItem -> (holder as MatrixListItemViewHolder).onBind(item.matrix)
-            is MatrixListItem.TitleItem -> (holder as MatrixListTitleViewHolder).onBind(item)
+            is MatrixListItem.HeaderItem -> (holder as MatrixListItemViewHolder.Header).onBind(item)
+            is MatrixListItem.MatrixItem -> (holder as MatrixListItemViewHolder.Matrix).onBind(item)
+            is MatrixListItem.TitleItem -> (holder as MatrixListItemViewHolder.Title).onBind(item)
         }
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return when (getItem(position)) {
-            is MatrixListItem.HeaderItem -> VT_HEADER
-            is MatrixListItem.MatrixItem -> VT_ITEM
-            is MatrixListItem.TitleItem -> VT_TITLE
-        }
+    override fun getItemViewType(position: Int) = when (getItem(position)) {
+        is MatrixListItem.HeaderItem -> VT_HEADER
+        is MatrixListItem.MatrixItem -> VT_ITEM
+        is MatrixListItem.TitleItem -> VT_TITLE
     }
 }
--- app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemViewHolder.kt
+++ app/src/main/java/kr/co/hs/sudoku/feature/matrixlist/MatrixListItemViewHolder.kt
@@ -1,15 +1,66 @@
 package kr.co.hs.sudoku.feature.matrixlist
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
 import kr.co.hs.sudoku.core.ViewHolder
-import kr.co.hs.sudoku.databinding.LayoutMatrixListItemBinding
-import kr.co.hs.sudoku.model.matrix.IntMatrix
+import kr.co.hs.sudoku.databinding.LayoutListItemMatrixBinding
+import kr.co.hs.sudoku.databinding.LayoutListItemMatrixHeaderBinding
+import kr.co.hs.sudoku.databinding.LayoutListItemMatrixTitleBinding
+import java.io.InvalidClassException
 
-class MatrixListItemViewHolder(
-    val binding: LayoutMatrixListItemBinding
-) : ViewHolder(binding.root) {
+sealed class MatrixListItemViewHolder<T : MatrixListItem>(itemView: View) : ViewHolder(itemView) {
+    companion object {
+        inline fun <reified T : MatrixListItem> create(
+            inflater: LayoutInflater,
+            parent: ViewGroup
+        ) = when (T::class.java) {
+            MatrixListItem.MatrixItem::class.java ->
+                Matrix(LayoutListItemMatrixBinding.inflate(inflater, parent, false))
 
-    fun onBind(item: IntMatrix) = with(binding.matrix) {
-        matrix = item
-        invalidate()
+            MatrixListItem.HeaderItem::class.java ->
+                Header(LayoutListItemMatrixHeaderBinding.inflate(inflater, parent, false))
+
+            MatrixListItem.TitleItem::class.java ->
+                Title(LayoutListItemMatrixTitleBinding.inflate(inflater, parent, false))
+
+            else -> throw InvalidClassException("invalid type")
+        }
+    }
+
+    abstract val clickableView: View
+
+    abstract fun onBind(item: T)
+
+    fun setOnClickListener(l: OnClickListener) = clickableView.setOnClickListener(l)
+
+    class Matrix(private val binding: LayoutListItemMatrixBinding) :
+        MatrixListItemViewHolder<MatrixListItem.MatrixItem>(binding.root) {
+
+        override val clickableView: View by lazy { binding.cardView }
+
+        override fun onBind(item: MatrixListItem.MatrixItem) = with(binding.matrix) {
+            matrix = item.matrix
+            invalidate()
+        }
+
+    }
+
+    class Title(private val binding: LayoutListItemMatrixTitleBinding) :
+        MatrixListItemViewHolder<MatrixListItem.TitleItem>(binding.root) {
+        override val clickableView: View by lazy { binding.tvTitle }
+        override fun onBind(item: MatrixListItem.TitleItem) = with(binding.tvTitle) {
+            text = item.title
+        }
+    }
+
+    class Header(private val binding: LayoutListItemMatrixHeaderBinding) :
+        MatrixListItemViewHolder<MatrixListItem.HeaderItem>(binding.root) {
+
+        override val clickableView: View by lazy { binding.cardView }
+        override fun onBind(item: MatrixListItem.HeaderItem) = with(binding.tvHeader) {
+            text = item.header
+        }
     }
 }
