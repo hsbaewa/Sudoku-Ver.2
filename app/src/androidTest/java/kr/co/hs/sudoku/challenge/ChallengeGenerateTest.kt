7a471bad0265cd75e9794557fc907ae6a0018384
hsbaewa <hsbaewa@gmail.com>
2024-01-07 02:38:42 +0900

    #117 챌린지 자동 생성 코드 수정

--- app/src/androidTest/java/kr/co/hs/sudoku/challenge/ChallengeGenerateTest.kt
+++ app/src/androidTest/java/kr/co/hs/sudoku/challenge/ChallengeGenerateTest.kt
@@ -2,9 +2,12 @@ package kr.co.hs.sudoku.challenge
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.runTest
 import kr.co.hs.sudoku.model.challenge.impl.ChallengeEntityImpl
+import kr.co.hs.sudoku.model.matrix.CustomMatrix
 import kr.co.hs.sudoku.repository.challenge.ChallengeRepositoryImpl
+import kr.co.hs.sudoku.usecase.AutoGenerateSudokuUseCase
 import kr.co.hs.sudoku.usecase.RandomCreateSudoku
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,16 +20,22 @@ class ChallengeGenerateTest {
 
     @Test
     fun generateChallenge() = runTest(timeout = Duration.INFINITE) {
-//        val challengeRepository = ChallengeRepositoryImpl()
-//
 //        val createMatrix = RandomCreateSudoku(9, 50.0).getIntMatrix()
+//        val sudoku = AutoGenerateSudokuUseCase(
+//            createMatrix.boxSize,
+//            createMatrix.boxCount,
+//            createMatrix
+//        ).invoke().last()
+//
+//        val resultTable = CustomMatrix(sudoku.toValueTable())
 //
 //        val challengeEntity = ChallengeEntityImpl(
-//            challengeId = "2024-01-01",
-//            matrix = createMatrix,
+//            challengeId = "2024-01-07",
+//            matrix = resultTable,
 //            createdAt = Date()
 //        )
 //
+//        val challengeRepository = ChallengeRepositoryImpl()
 //        val result = challengeRepository.createChallenge(challengeEntity)
 //        assertTrue(result)
 
