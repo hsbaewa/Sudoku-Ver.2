469b09a59c44acb9c95f8104760622be848e30bc
hsbaewa <hsbaewa@gmail.com>
2024-02-22 16:00:05 +0900

    #226 사용자 프로필 도전 기록 아이템 을 통해서도 도전 시작 가능하도록 개선.

---
+++ app/src/main/java/kr/co/hs/sudoku/core/BottomSheetDialogFragment.kt
@@ -0,0 +1,34 @@
+package kr.co.hs.sudoku.core
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BottomSheetDialogFragment : BottomSheetDialogFragment() {
+    inline fun showAlert(
+        titleResId: Int? = null,
+        msgResId: Int,
+        crossinline onClosed: () -> Unit
+    ) = (requireActivity() as Activity).showAlert(titleResId, msgResId, onClosed)
+
+    inline fun showAlert(
+        title: String? = null,
+        msg: String,
+        crossinline onClosed: () -> Unit
+    ) = (requireActivity() as Activity).showAlert(title, msg, onClosed)
+
+    inline fun showConfirm(
+        titleResId: Int? = null,
+        msgResId: Int,
+        crossinline onConfirm: (Boolean) -> Unit
+    ) = (requireActivity() as Activity).showConfirm(titleResId, msgResId, onConfirm)
+
+    inline fun showConfirm(
+        title: String? = null,
+        msg: String,
+        crossinline onConfirm: (Boolean) -> Unit
+    ) = (requireActivity() as Activity).showConfirm(title, msg, onConfirm)
+
+    protected fun Throwable.showErrorAlert() = showAlert(
+        requireActivity().title.toString(),
+        (requireActivity() as Activity).getErrorMessage(this)
+    ) {}
+}
